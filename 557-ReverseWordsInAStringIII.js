1af4b85a7f72190be17541cbfaeaa6a026d727c9
Andrew Duong <duongandrews@gmail.com>
2020-08-05 23:14:11 -0400

    Refactored - split words into array, used map method to split each word into characters and each word into an array within an array, and reversed each character and joined them back

--- 557-ReverseWordsInAStringIII.js
+++ 557-ReverseWordsInAStringIII.js
@@ -12,21 +12,25 @@ Note: In the string, each word is separated by single space and there will not b
  * @return {string}
  */
 var reverseWords = function (s) {
-  let stringOutput = "";
-  let splitArr = s.split(" ");
-  if (s.length === 0) {
-    return "";
-  }
-  for (let i = 0; i < splitArr.length; i++) {
-    for (let j = splitArr[i].length - 1; 0 <= j; j--) {
-      stringOutput += splitArr[i][j];
-      if (j === 0 && i === splitArr.length - 1) {
-        return stringOutput;
-      } else if (j === 0) {
-        stringOutput += " ";
-      }
-    }
-  }
+  // let stringOutput = "";
+  // let splitArr = s.split(" ");
+  // if (s.length === 0) {
+  //   return "";
+  // }
+  // for (let i = 0; i < splitArr.length; i++) {
+  //   for (let j = splitArr[i].length - 1; 0 <= j; j--) {
+  //     stringOutput += splitArr[i][j];
+  //     if (j === 0 && i === splitArr.length - 1) {
+  //       return stringOutput;
+  //     } else if (j === 0) {
+  //       stringOutput += " ";
+  //     }
+  //   }
+  // }
+  return s
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
 };
 
 let string = "Let's take LeetCode contest";
