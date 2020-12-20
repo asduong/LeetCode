841ae2d1e1e00729118f0971d899c6deddb9d0ca
Andrew Duong <duongandrews@gmail.com>
2020-12-20 00:09:04 -0500

    Divided for loop length by 2, created new empty array and shuffled the array using n

---
+++ 1470-ShuffleTheArray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+const shuffle = (nums, n) => {
+  const jump = n;
+  const shuffledArr = [];
+  if ((n = 0)) {
+    return nums;
+  }
+  for (let i = 0; i < nums.length / 2; i++) {
+    shuffledArr.push(nums[i]);
+    shuffledArr.push(nums[i + jump]);
+  }
+  return shuffledArr;
+};
+
+const nums = [1, 1, 2, 2];
+const n = 2;
+console.log(shuffle(nums, n));
