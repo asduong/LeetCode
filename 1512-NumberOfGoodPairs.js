a689d3357961610fa6ad7b81c51fb280aefa3fcf
Andrew Duong <duongandrews@gmail.com>
2020-12-21 15:16:59 -0500

    Double for loop to compare original number with every other number after the original number, if equal, increment good pairs

---
+++ 1512-NumberOfGoodPairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const numIdenticalPairs = (nums) => {
+  let goodPairs = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        goodPairs += 1;
+      }
+    }
+  }
+  return goodPairs;
+};
+
+const nums = [1, 1, 1, 1];
+console.log(numIdenticalPairs(nums));
